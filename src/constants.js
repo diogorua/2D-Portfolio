c3e523a268950baff00489884c1973a039fa122e
Diogo Rua <diogorua316@gmail.com>
2025-06-26 22:31:40 +0100

    update full project

---
+++ src/constants.js
@@ -0,0 +1,21 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+    pc: `This is my PC. I use it to work and develop some projects in Java, Scala, Python, PHP. Recently and with the help of this project and FreeCodeCamp, I have been learning JavaScript.
+    Anyway regardless of the language, I just like programming.`,
+    "degree": `This is my ISTA Top Talent degree. I hung it on the wall because I'm proud of it.
+    Regarded as one of the best excelling students by ISTA (Technology and Architecture School) during the year 23/24.`,
+    "sofa-table": `That's my sofa. I like to relax here and watch some movies or youtube or even a good sleep.`,
+    tv: `That's my TV. I have been watching tech youtubers and tech channels. Watching tech youtubers might be very inspiring and motivating because they share their experiences and knowledge about technology, which can help you learn and grow in the field.
+    Tech channels often provide such a good tutorials which helped me to develop my skills and also develop some of my projects.`,
+    bed: `This where I sleep. We have to agree that nothing beats a good sleep after a long day of work and study.`,
+    resume: `This is my desk and on it is my resume, <a href="Resume_DiogoRua.pdf" target="_blank">check it out.</a>.
+    Contact me at diogorua316@gmail.com if you have any interesting job opportunities!`,
+    projects: `Info about this portfolio: It's made with the Kaboom.js library which is a library for making games in JavaScript.
+    Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas. 
+    You can check my <a href="https://github.com/diogorua" target="_blank">GitHub</a> profile to see more.`,
+    library: `Behind my deep interest in engineering, I also like to read books about investments. I started to have interest in this area since I read the book "Comeca Ja" by Pedro Andersson.
+    It helped me to understand the importance of financial education and how to manage my money wisely. I already started to invest in the stock market and I am learning more about it every day.
+    The biggest lesson I learned is that you can start with a few amount of money and still make a difference in your financial future. The key is to start early and be consistent.`, 
+    exit: `I hope you enjoyed this little tour around my 2D portfolio. If you want to exit, just close the tab. See you next time!`,
+};
